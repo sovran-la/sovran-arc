38b9ce504abc5faf7fb6394bcee79444c473b9d6
Brandon Sneed <brandon@redf.net>
2024-11-12 22:04:16 -0800

    updated release script.

--- release.sh
+++ release.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# if anything fails, we don't release.
+set -euo pipefail
+
+# Run all tests
+echo "Running tests..."
+cargo test
+
+# Run all examples
+echo "Running examples..."
+cargo run --examples
+
+# Run release script
+echo "Running release process..."
 cargo run --bin release
